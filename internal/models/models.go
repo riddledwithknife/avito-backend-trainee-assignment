dbd5ac637268743b0f5d22e6e87290fe5818866c
riddledwithknife <riddledwithknife@icloud.com>
2024-04-13 13:36:30 +0300

    [upd] The basic part is ready, there is still paling, tests and to check full compliance with the api

---
+++ internal/models/models.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type Config struct {
+	Database struct {
+		Host     string `json:"host"`
+		Port     string `json:"port"`
+		User     string `json:"user"`
+		Password string `json:"password"`
+		DBName   string `json:"dbName"`
+	} `json:"database"`
+}
+
+type Banner struct {
+	ID        uint          `gorm:"primaryKey" json:"id"`
+	FeatureID uint          `gorm:"not null" json:"feature_id"`
+	TagIDs    pq.Int64Array `gorm:"type:integer[]" json:"tag_ids"`
+	Title     string        `gorm:"not null" json:"title"`
+	Text      string        `gorm:"not null" json:"text"`
+	URL       string        `gorm:"not null" json:"url"`
+	IsActive  bool          `gorm:"default:true" json:"is_active"`
+	CreatedAt time.Time     `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time     `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+type Token struct {
+	ID      uint   `gorm:"primaryKey" json:"id"`
+	Token   string `gorm:"not null;unique" json:"token"`
+	IsAdmin bool   `gorm:"not null" json:"is_admin"`
+}
